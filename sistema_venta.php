00f15960f135ef695bc592cfbb92705c077cff42
Hrojas <rojasvasquezhilder@gmail.com>
2020-08-19 12:19:43 -0500

    primer avance con php

---
+++ sistema_venta.php
@@ -0,0 +1,16 @@
+<html> 
+ <head> 
+   <title>Nuestro primer proyecto en php, css y javaScript</title> 
+ </head> 
+ 
+  <body bgcolor="#5ff4ff">
+    </body>
+
+  <?php
+ 
+ echo "<h1><center>¡Hola mundo este es ! <br></center><h1>";
+  echo "<h2>  mi proyecto de sistema de ventas de programacion web </h2>";
+ 
+ ?>
+
+  </html>
