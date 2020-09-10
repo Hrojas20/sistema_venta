bd798636599b9c902c5aa7c02488bc3eccb04e65
Hrojas <rojasvasquezhilder@gmail.com>
2020-09-10 18:22:08 -0500

    ..

--- validar.php
+++ validar.php
@@ -1,20 +1,46 @@
+
 <?php
-  if(isset($_GET['us'])){
-    $usum=$_GET['us'];
-    if($usum=="usuarioincorrecto"){
 
-    }
-    ?>
-  <p> <font color="#FF0000"> <? echo "usuario no existe";?>
-  </font>
-  </p>
-  <?php
-   }
-  if($usum =="contraseñaincorrecta")
-  {
+session_start();
+	require("conec.php");
 
-  
-  ?>
-  <p>
-  <font color="#FF0000"> <? echo "la contraseña no conrresponde"; ?> </font>
-  </p>
+	$username=$_POST['mail'];
+	$pass=$_POST['pass'];
+
+
+	$sql2=mysqli_query($mysqli,"SELECT * FROM login WHERE email='$username'");
+	if($f2=mysqli_fetch_assoc($sql2)){
+		if($pass==$f2['pasadmin']){
+			$_SESSION['id']=$f2['id'];
+			$_SESSION['user']=$f2['user'];
+			$_SESSION['rol']=$f2['rol'];
+
+			echo '<script>alert("ADMINISTRADOR")</script> ';
+			echo "<script>location.href='crudadmin.php'</script>";
+		
+		}
+	}
+
+
+	$sql=mysqli_query($mysqli,"SELECT * FROM login WHERE email='$username'");
+	if($f=mysqli_fetch_assoc($sql)){
+		if($pass==$f['password']){
+			$_SESSION['id']=$f['id'];
+			$_SESSION['user']=$f['user'];
+			$_SESSION['rol']=$f['rol'];
+
+			header("Location: index1.php");
+		}else{
+			echo '<script>alert("CONTRASEÑA INCORRECTA")</script> ';
+		
+			echo "<script>location.href='registro.php'</script>";
+		}
+	}else{
+		
+		echo '<script>alert("CUENTA NO EXISTENTE/REGISTRATE")</script> ';
+		
+		echo "<script>location.href='registro.php'</script>";	
+
+	}
+
+?>
